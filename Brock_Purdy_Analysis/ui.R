459ef14002441e73432b21b9ba7273d84d4166b4
alexanderjireland <irelandalexander1@gmail.com>
2025-02-04 17:29:52 -0600

    added comments to help guide direction for ui

--- Brock_Purdy_Analysis/ui.R
+++ Brock_Purdy_Analysis/ui.R
@@ -22,13 +22,25 @@ fluidPage(
                   "Select Quarterback",
                   choices = qb_clean |> distinct(passer_player_name) |> pull(passer_player_name) |> sort(),
                   selected = "B.Purdy")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Model vs. Reality", plotOutput("plot_model_actual")),
-      )
+    )
+  ),
+  #work on creating a selection for dependent var
+  
+  #select coach
+  #select with and without rb
+  
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Model vs. Reality", plotOutput("plot_model_actual"))
+      
     )
   )
 )
+
+
+#qb pressure sensitivity
+#QB performance with high YAC games
+#3d scatterplot
+#box plot for each ind. var and 3 dep var
